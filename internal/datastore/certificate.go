2f1f29d5be58c0e6579035d4322e4b2df17f90d9
sarthak0714 <sarthak.t1403@gmail.com>
2024-07-29 00:22:05 +0530

    crud ops for certificates

--- internal/datastore/certificate.go
+++ internal/datastore/certificate.go
@@ -1,16 +1,77 @@
 package datastore
 
 import (
+	"context"
+	"time"
+
 	"github.com/sarthak0714/certisure-go/internal/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type CertificateDatastore interface {
 	Create(cert *models.Certificate) error
+	Delete(id string) error
 	GetById(id string) (*models.Certificate, error)
 	GetByGroup(groupId string) ([]*models.Certificate, error)
 }
 
 type MongoCertificateDatastore struct {
-	db *mongo.Database
+	collection *mongo.Collection
+}
+
+func NewMongoCertificateDatastore(db *mongo.Database) *MongoCertificateDatastore {
+	return &MongoCertificateDatastore{collection: db.Collection("certificates")}
+
+}
+
+func (datastore *MongoCertificateDatastore) GetById(id string) (*models.Certificate, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var cert models.Certificate
+	err := datastore.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&cert)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cert, nil
+}
+
+func (datastore *MongoCertificateDatastore) GetByGroup(group_id string) ([]*models.Certificate, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := datastore.collection.Find(ctx, bson.M{"group_id": group_id})
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(ctx)
+	var certs []*models.Certificate
+
+	for cursor.Next(ctx) {
+		var cert models.Certificate
+		if err = cursor.Decode(&cert); err != nil {
+			return nil, err
+		}
+		certs = append(certs, &cert)
+	}
+
+	if err = cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return certs, nil
+}
+
+func (datastore *MongoCertificateDatastore) Delete(id string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_, err := datastore.collection.DeleteOne(ctx, bson.M{"_id": id})
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internal/models/certificate.go
+++ internal/models/certificate.go
@@ -1,16 +1,10 @@
 package models
 
-import (
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type Certificate struct {
-	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	GroupId   string             `bson:"group_id" json:"group_id"`
-	GroupName string             `bson:"group_name" json:"group_name"`
-	Email     string             `bson:"email" json:"email"`
-	Username  string             `bson:"username" json:"username"`
-	Date      time.Time          `bson:"data" json:"data"`
+	Id        string `bson:"_id,omitempty" json:"_id"`
+	GroupId   string `bson:"group_id" json:"group_id"`
+	GroupName string `bson:"group_name" json:"group_name"`
+	Email     string `bson:"email" json:"email"`
+	Username  string `bson:"username" json:"username"`
+	Date      string `bson:"data" json:"data"`
 }
